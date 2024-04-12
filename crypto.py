098ee12708fc3b435223616c158a35624b2b861a
Maurits Brinkman <maurits.brinkman@asml.com>
2024-04-12 11:20:56 +0200

    Completed example on simple mean-reverting with couple of crypto assets.

--- crypto.py
+++ crypto.py
@@ -1,6 +1,11 @@
 import yfinance as yf
 import numpy as np
 from scipy.linalg import sqrtm
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib
+matplotlib.use('TkAgg')
+sns.set(style="whitegrid", context="notebook")
 
 # List of major cryptocurrencies
 major_crypto_symbols = ['BTC-EUR', 'ETH-EUR', 'LTC-EUR', 'XRP-EUR']
@@ -18,9 +23,9 @@ for symbol in major_crypto_symbols:
         hourly_data[symbol] = hourly_data[symbol][:min_length]
 
 # Rescale features by setting mean to zero and obtain asset matrix S_t
-S = np.vstack(list(hourly_data.values()))
-mean = np.nanmean(S, axis=0)
-S -= mean
+S_original = np.vstack(list(hourly_data.values()))
+mean = np.nanmean(S_original, axis=1)
+S = S_original - mean[:, np.newaxis]
 
 # Fit A matrix using least squares (note that '@' is equivalent to using np.matmul)
 A = np.linalg.inv(S[:, :-1] @ S[:, :-1].T) @ (S[:, :-1] @ S[:, 1:].T)
@@ -36,6 +41,57 @@ B = C_sqrt_inv @ A.T @ C @ A @ C_sqrt_inv
 # Get eigenvalues and eigenvectors of B
 eig = np.linalg.eig(B)
 eig_values, eig_vectors = eig[0], eig[1]
+
+# First eigenvalue is smallest, second eigenvalue is largest
+z_mr = eig_vectors[:, 0]  # Mean reversion vector
+z_mom = eig_vectors[:, 1]  # Momentum vector
+
+# Compute weights
+x_mr = np.array([C_sqrt_inv @ z_mr]).T  # Weights if goal is optimal mean reversion
+x_mom = np.array([C_sqrt_inv @ z_mom]).T  # Weights if goal is optimal momentum
+
+# Normalize weights
+x_mr /= sum(abs(x_mr[:, 0]))
+x_mom /= sum(abs(x_mom[:, 0]))
+
+# Construct the portfolio (still having 0 mean)
+P_mr = (x_mr.T @ S)[0]
+P_mom = (x_mom.T @ S)[0]
+
+# Re-mean our assets
+P_mr_value = [1]
+P_mom_value = [1]
+for i in range(len(S[0])-1):
+    rets = (S_original[:, i+1] - S_original[:, i])/S_original[:, i]
+
+    P_mr_value.append(P_mr_value[i] * (1 + (rets @ x_mr)[0]))
+    P_mom_value.append(P_mom_value[i] * (1 + (rets @ x_mom)[0]))
+
+# Create figure with four subplots
+fig, axes = plt.subplots(2, 2)
+time_axis = data.T.T.index[:min_length]
+
+axes[0, 0].plot(time_axis, P_mr, color='navy')
+axes[0, 0].set_title('Optimal Mean-Reverting Portfolio (Demeaned)', fontweight='bold')
+
+axes[0, 1].plot(time_axis, P_mom, color='navy')
+axes[0, 1].set_title('Optimal Predictable Portfolio (Demeaned)', fontweight='bold')
+
+axes[1, 0].plot(time_axis, P_mr_value, color='navy')
+axes[1, 0].set_title('Optimal Mean-Reverting Portfolio', fontweight='bold')
+
+axes[1, 1].plot(time_axis, P_mom_value, color='navy')
+axes[1, 1].set_title('Optimal Predictable Portfolio', fontweight='bold')
+
+for ax in axes.flatten():
+    ax.set_xticks(ax.get_xticks())
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right', va='top')
+    ax.set_ylabel('Portfolio Value')
+
+plt.tight_layout()
+plt.show()
+
+
 
 
 
