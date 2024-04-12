a9247a7e60ed6aa2b60d5ad3db34f6839d3b8964
Maurits Brinkman <maurits.brinkman@asml.com>
2024-04-12 16:23:06 +0200

    Completed Greedy Search algorithm.

--- greedy_search.py
+++ greedy_search.py
@@ -1,55 +1,68 @@
 import numpy as np
 from itertools import combinations
 import yfinance as yf
+from utils import compute_weights
 from scipy.linalg import sqrtm
 
-best_assets = [None, None]
-best_pred = np.inf
-best_x = None
+
+def greedy_search(tickers, k, a=5):
+    best_assets = [None, None]
+    best_pred = np.inf
+    best_x = None
+
+    # Generate all combinations of two assets
+    assets = yf.download(tickers, start='2022-01-01', end='2024-01-01')['Adj Close']
+    asset_combinations = combinations(assets, 2)
+
+    for i in range(2, k+1):
+        if i == 2:
+            # Find the best two assets
+            for asset_pair in asset_combinations:
+                asset1, asset2 = asset_pair
+
+                # Extract the adjusted closing prices for the two assets
+                cols = [asset1, asset2]
+                prices = assets[cols]
+
+                S = np.array(prices).T
+                x, A, C, C_sqrt_inv, eig_values, eig_vectors = compute_weights(S)
+
+                if max(abs(x)) <= 0.8:
+                    pred = (x.T @ A.T @ C @ A @ x) / (x.T @ C @ x)
+
+                    if pred < best_pred:
+                        best_pred = pred
+                        best_assets = [asset1, asset2]
+                        best_x = x
+        else:
+            for j in range(len(assets.columns)):
+                asset = assets.columns[j]
+                if asset not in best_assets:
+                    prev_assets = np.array(assets[best_assets[:i]]).T
+                    current_asset = np.array(assets[asset]).T
+
+                    S = np.vstack((prev_assets, current_asset))
+                    x, A, C, C_sqrt_inv, eig_values, eig_vectors = compute_weights(S)
+
+                    if max(abs(x)) <= 0.8 and min(abs(x)) > 1 / (a * k):
+                        pred = (x.T @ A.T @ C @ A @ x) / (x.T @ C @ x)
+
+                        if pred < best_pred:
+                            best_pred = pred
+                            best_assets = best_assets[:i] + [asset]
+                            best_x = x
+
+    return best_assets, best_x, best_pred
+
 
 # Define a list of tickers or symbols for the assets you're interested in
-tickers = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'TSLA', 'NFLX', 'NVDA', 'AMD', 'INTC', 'CSCO', 'ADBE', 'PYPL', 'CRM',
-           'IBM', 'ORCL']
+ticker_names = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'TSLA', 'NFLX', 'NVDA', 'AMD', 'INTC', 'CSCO', 'ADBE', 'PYPL', 'CRM',
+                'IBM', 'ORCL']
 
-# Generate all combinations of two assets
-assets = yf.download(tickers, start='2022-01-01', end='2024-01-01')['Adj Close']
-asset_combinations = combinations(assets, 2)
+# Set number of assets for sparse portfolio and minimum portfolio weight
+k, a = 8, 4
 
-for asset_pair in asset_combinations:
-    asset1, asset2 = asset_pair
-
-    # Extract the adjusted closing prices for the two assets
-    cols = [asset1, asset2]
-    prices = assets[cols]
-
-    S = np.array(prices).T
-
-    A = np.linalg.inv(S[:, :-1] @ S[:, :-1].T) @ (S[:, :-1] @ S[:, 1:].T)
-
-    C = np.cov(S)
-    C_sqrt = sqrtm(C)
-    C_sqrt_inv = np.linalg.inv(C_sqrt)
-
-    B = C_sqrt_inv @ A.T @ C @ A @ C_sqrt_inv
-
-    eig = np.linalg.eig(B)
-    eig_values, eig_vectors = eig[0], eig[1]
-
-    index = np.argmin(eig_values)
-    z = eig_vectors[:, index]
-
-    x = np.array([C_sqrt_inv @ z]).T
-    x /= sum(abs(x[:, 0]))
-
-    if max(abs(x)) <= 0.8:
-        pred = (x.T @ A.T @ C @ A @ x) / (x.T @ C @ x)
-
-        if pred < best_pred:
-            best_pred = pred
-            best_assets = [asset1, asset2]
-            best_x = x
-
+best_assets, best_x, predictive_value = greedy_search(tickers=ticker_names, k=k, a=a)
 print('Best assets: ', best_assets)
 print('Portfolio weights: ', best_x)
-print('Predictability: ', best_pred)
-
+print('Predictability: ', predictive_value)
